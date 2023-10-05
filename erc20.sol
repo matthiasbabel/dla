586269d9bbddfe028478feb3b250460f08c6b3a8
Matthias Babel <34661018+matthiasbabel@users.noreply.github.com>
2023-10-05 11:50:01 +0200

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -10,11 +10,16 @@ pragma solidity >=0.8.2 <0.9.0;
 contract OffSetting {
 
     uint public totalSupply;
+    address owner;
+    mapping(address => bool) public allowancesMint;
+    mapping(address => uint256) public prices;
     mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) public allowancesBurn;
 
     constructor() {
         totalSupply = 100;
         balances[msg.sender] = totalSupply;
+        owner = msg.sender;
     }
 
     function balanceOf(address _owner) public view returns (uint256) {
@@ -30,4 +35,60 @@ contract OffSetting {
         
         return true;
     }
+
+    // Task3 - Trading of offsets
+    // Set Price
+    function setPrice(uint256 _price) public {
+        prices[msg.sender] = _price
+    }
+
+    // Task3
+    // Buy offsets. if price = 0: Buying not possible
+    function buy(address payable _from) public payable {
+        require(prices[_from] != 0; "Not possible");
+        uint256 amount = msg.value \ price;
+        require(balances[_from] <= amount, "Not enough offsets");
+        
+        balances[msg.sender] += _amount;
+        balances[_from] -= _amount;
+        //ggf. ersetzen mit "Call" https://solidity-by-example.org/sending-ether/
+        _from.send(msg.value);
+    }
+
+    // Task2 
+    // Allow selected parties to mint offsets (only by contract owner)
+    function allowMinting(address _minter) public {
+        require(msg.sender == owner, "No permission");
+        allowancesMint[_minter] = true;
+    }
+
+    // Task2
+    // Implement Minting for authorized addresses
+    function mint(uint256 _amount) public {
+        require(allowancesMint[msg.sender], "No allowance for minting");
+        balances[msg.sender] += _amount;
+    }
+
+    function burn(uint256 _amount) public {
+        balances[msg.sender] -= _amount;
+    }
+
+    // Task 1
+    // Give some third party the allowance to burn your offset 
+    function addAllowBurn(address _burner, uint256 _amount) public returns (uint256) {
+        allowancesBurn[msg.sender][_burner] = _amount;
+        return allowancesBurn[msg.sender][_burner];
+    }
+
+    // Task 1
+    // Burn Offset of someone else
+    function burnFrom(address _from, uint256 _amount) public returns (uint256) {
+        require(allowancesBurn[_from][msg.sender] >= _amount, "No allowance");
+        allowancesBurn[_from][msg.sender] -= _amount;
+        return allowancesBurn[_from][msg.sender];
+    }
+
+    // Task 4
+    // Add a Struct, which logs all minting events. Amount, Minter, Description
+    // TODO: Implement
 }
